f4b94be151e163db8c9d6fa1a342d8e1510c809a
Hellen Jasmine <hellen05jasmine@icloud.com>
2024-07-19 10:25:44 -0300

    injetando valores com o context_processors nos templates

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'site_setup.context_processors.context_processor_example',
+                'site_setup.context_processors.site_setup',
             ],
         },
     },
