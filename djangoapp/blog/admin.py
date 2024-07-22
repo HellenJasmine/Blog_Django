e4acdbe69a441a33c6eb6855d8b83d4c9fe90e3d
Hellen Jasmine <hellen05jasmine@icloud.com>
2024-07-22 10:49:04 -0300

    adicionando o model Page

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -23,3 +23,16 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         'slug':('name',),
     }
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'is_published',
+    list_display_links = 'title',
+    search_fields = 'id', 'slug', 'title', 'content',
+    list_per_page = 50
+    list_filter = 'is_published',
+    list_editable = 'is_published',
+    ordering = '-id',
+    prepopulated_fields = {
+        "slug": ('title',),
+    }
