6b63da1b24034fb3e3e4ecf3a16c0f44669eaddf
Hellen Jasmine <hellen05jasmine@icloud.com>
2024-07-22 08:46:01 -0300

    adicionando layout no blog

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin 
 from django.urls import path
-from blog.views import index
+from blog.views import *
 
 
 app_name = 'blog'
 
 urlpatterns = [
-    path("", index, name='index')
+    path("", index, name='index'),
+    path("page/", page, name='page'),
+    path("post/", post, name='post'),
 ]
