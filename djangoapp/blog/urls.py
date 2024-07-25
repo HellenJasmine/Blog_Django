506eed02bbcd932bdc601dbf2dc4cf8636630377
Hellen Jasmine <hellen05jasmine@icloud.com>
2024-07-25 09:14:49 -0300

    Usando self.kwargs para argumentos de URL e ordem de chamada dos métodos em CBV

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("page/<slug:slug>/", page, name='page'),
     path("post/<slug:slug>/", post, name='post'),
 
-    path("created_by/<int:author_pk>/", created_by, name='created_by'),
+    path("created_by/<int:author_pk>/", CreatedByListView.as_view(), name='created_by'),
     path("category/<slug:slug>/", category, name='category'),
     path('tag/<slug:slug>/', tag, name='tag'),
     path('search/', search, name="search"),
